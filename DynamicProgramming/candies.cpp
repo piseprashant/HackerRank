bd74310bb52357fa9ab85450e59ffaa8f91c01ac
Prashant <piseprashant@johndeere.com>
2021-09-15 10:33:58 +0530

    candies

---
+++ DynamicProgramming/candies.cpp
@@ -0,0 +1,50 @@
+// https://www.youtube.com/watch?v=h6_lIwZYHQw
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+//#include<bits/stdc++.h>
+
+using namespace std;
+
+
+long candies( int n, vector<int> arr) {
+
+    vector<int> candies(n,0);
+    
+
+    candies[0] = 1;
+    for (int i = 1; i < arr.size(); i++)
+        if (arr[i - 1] < arr[i])
+            candies[i] = candies[i - 1] + 1;
+        else
+            candies[i] = 1;
+
+
+    long sumCandies = candies[arr.size() - 1];
+    for (int i = arr.size() - 2; i >= 0; i--)
+    {
+        if (arr[i] > arr[i + 1])
+            candies[i] = max(candies[i + 1] + 1, candies[i]);
+        sumCandies += candies[i];
+    }
+
+    return sumCandies;
+}
+
+
+
+int main()
+{
+    vector<int> arr = { 8, 2, 4, 3, 5, 2, 6, 4, 5 };
+
+	std::cout << candies(arr.size(), arr) << std::endl;
+
+	return 0;
+}
+
+
